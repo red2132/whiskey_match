d5bd4c06adc0c91e00414f22ecadbc9fc24b695b
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-11-12 23:46:29 +0900

    리뷰 개수 폰트 사이즈 조정

--- components/detail/RatingDetail.tsx
+++ components/detail/RatingDetail.tsx
@@ -5,7 +5,7 @@ export default function RatingDetail() {
   return (
     <div className="flex flex-col gap-1 md:gap-3 relative w-3/4">
       <h1 className="main-text">위스키 궁합 점수</h1>
-      <div className="flex justify-end pb-5 text-sm sm:text-xl">
+      <div className="flex justify-end pb-5 text-sm md:text-xl">
         <p>리뷰 10개</p>
       </div>
       <div className="flex flex-col justify-center items-center gap-2 md:gap-3">
